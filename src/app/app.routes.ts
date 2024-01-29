6d6eca3a65486067c7e971000b4cc17c41451032
Ricardo CD <rcd9108@hotmail.com>
2024-01-29 18:33:48 +0100

    Adding layout component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,17 @@ import {HomeComponent} from './pages/home/home.component';
 import {LabsComponent} from './pages/labs/labs.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { NotFoundComponent } from '@components/not-found/not-found.component';
+import { LayoutComponent } from '@components/layout/layout.component';
 
 export const routes: Routes = [
-    { path : '',  component : HomeComponent },
-    { path : 'home',  component : HomeComponent },
-    { path : 'products',  component : ProductListComponent },
-    { path : 'labs',  component : LabsComponent },
+    { 
+        path : '',  
+        component : LayoutComponent,
+        children:[
+            { path : 'home',  component : HomeComponent },
+            { path : 'products',  component : ProductListComponent },
+            { path : 'labs',  component : LabsComponent },
+        ] 
+    },
     { path : '**',  component : NotFoundComponent }
 ];
